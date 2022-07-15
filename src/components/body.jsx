030b6a1881d7f7112e0677cfec3a7885e207f3c5
Shevja <vlsdki@gmail.com>
2022-07-15 22:19:28 +0700

    Changed api (OpenWeatherMap -> Weatherbit) and added styles (tiles)

--- src/components/body.jsx
+++ src/components/body.jsx
@@ -6,26 +6,21 @@ import './assets/body.css'
 import './assets/info.css'
 import './assets/weather.css'
 
-const API_KEY = "c642b6f7c81d3c42f61081b23d3bded5"
+const API_KEY = 'e77278944be14c17ae9cd4bc737934a0';
+// const API_KEY = "c642b6f7c81d3c42f61081b23d3bded5"
 
 class Body extends Component {
 
     state = {
         city: undefined,
-        temp: undefined,
-        country: undefined,
-        sunrise: undefined,
-        sunset: undefined,
-        windSpeed: undefined,
-        pressure: undefined,
-        humidity: undefined,
-        event: undefined,
+        weatherInfo: undefined,
         error: undefined
     }
 
-    getData = async (city) => {
+    getDataFromFetch = async (city) => {
         let data;
-        const api_url = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`)
+        const api_url = await fetch(`https://api.weatherbit.io/v2.0/forecast/daily?city=${city}&key=${API_KEY}`);
+        // const api_url = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`)
         if (api_url.status === 200) {
             data = await api_url.json();
         } else {
@@ -34,57 +29,72 @@ class Body extends Component {
         return data;
     }
 
-    getTimeFromTimeStamp = (timeStamp) => {
-        let date = new Date();
-        date.setTime(timeStamp);
-        return date.getHours() + ':' + date.getMinutes();
+    getTimeFromTimeStamp = (timeStamp) => { // Криво работает
+        const date = new Date(timeStamp * 1000);
+        let dateOffset = (new Date().getTimezoneOffset()) / 60;
+        
+        // console.log(dateOffset / 60);
+        // console.log(date.toUTCString());
+
+        let hours = date.getUTCHours() + dateOffset < 0 ? 24 + dateOffset : date.getUTCHours() + dateOffset;
+        return hours + ':' + date.getUTCMinutes();
     }
 
-    getWeatherInfo = async (e) => {
+    getWeatherInfoFromResponseData = (weatherData) => {
+        let weather16days = [];
+        weatherData.forEach(element => {
+            weather16days.push({
+                temperature: {
+                    curr: element.temp,
+                    max: element.high_temp,
+                    min: element.low_temp,
+                    feels: element.app_min_temp
+                },
+                wind: {
+                    direction: element.wind_cdir,
+                    speed: element.wind_spd,
+                    gust: element.wind_gust_spd
+                },
+                sun: {
+                    rise: this.getTimeFromTimeStamp(element.sunrise_ts),
+                    set: this.getTimeFromTimeStamp(element.sunset_ts)
+                },
+                phenomen: element.weather.description,
+                pressure: element.pres,
+                humidity: element.rh,
+                clouds: element.clouds,
+                uv: element.uv,
+                precipitations: element.pop,
+                date: element.datetime
+            })
+        });
+        return weather16days;
+    }
+
+    getWeatherInfoByCity = async (e) => {
         e.preventDefault();
         const city = e.target.elements.city.value;
 
         if (city) {
-            const data = await this.getData(city);
-
+            const data = await this.getDataFromFetch(city);
             if (data) {
+                const weatherInfo = this.getWeatherInfoFromResponseData(data.data);
                 this.setState({
-                    city: data.name,
-                    temp: Math.round(data.main.temp),
-                    country: data.sys.country,
-                    sunrise: this.getTimeFromTimeStamp(data.sys.sunrise),
-                    sunset: this.getTimeFromTimeStamp(data.sys.sunset),
-                    windSpeed: data.wind.speed,
-                    pressure: data.main.pressure,
-                    humidity: data.main.humidity,
-                    event: data.weather[0].main,
+                    city: data.city_name,
+                    weatherInfo: weatherInfo,
                     error: undefined
                 })
             } else {
                 this.setState({
                     city: undefined,
-                    temp: undefined,
-                    country: undefined,
-                    sunrise: undefined,
-                    sunset: undefined,
-                    windSpeed: undefined,
-                    pressure: undefined,
-                    humidity: undefined,
-                    event: undefined,
+                    weatherInfo: undefined,
                     error: "Введите название существующего города"
                 })
             }
         } else {
             this.setState({
                 city: undefined,
-                temp: undefined,
-                country: undefined,
-                sunrise: undefined,
-                sunset: undefined,
-                windSpeed: undefined,
-                pressure: undefined,
-                humidity: undefined,
-                event: undefined,
+                weatherInfo: undefined,
                 error: "Введите название города"
             })
         }
@@ -96,21 +106,10 @@ class Body extends Component {
                 <div className="main">
                     <div className="col-sm-4 info-container">
                         <Info />
-                        <Form weatherMethod={this.getWeatherInfo} />
+                        <Form weatherMethod={this.getWeatherInfoByCity} />
                     </div>
                     <div className="col-sm-8 weather-container">
-                        <Weather
-                            city={this.state.city}
-                            temp={this.state.temp}
-                            country={this.state.country}
-                            sunrise={this.state.sunrise}
-                            sunset={this.state.sunset}
-                            humidity={this.state.humidity}
-                            pressure={this.state.pressure}
-                            windSpeed={this.state.windSpeed}
-                            event={this.state.event}
-                            error={this.state.error}
-                        />
+                        <Weather weather={this.state} />
                     </div>
                 </div>
             </div>
--- src/components/elements/weather.jsx
+++ src/components/elements/weather.jsx
@@ -1,52 +1,90 @@
 import React from "react";
+import '../functions/weatherEventIcon'
 import '../assets/weather.css'
+import WeatherEventIcon from "../functions/weatherEventIcon";
+import WeatherPararmeterIcon from "../functions/weatherParameterIcon";
 
-const Weather = props => (
-    <div>
-        {props.city &&
-            <div className="weather-container">
-                <p>{props.city}, {props.country}</p>
-                <div className="temperature-info">
-                    <p style={{ fontSize: '72px' }}>{props.temp} C°</p>
-                    <p>{props.event}</p>
+const Weather = props => {
+    console.log(props.weather)
+
+    return (
+        <div>
+            {props.weather.city &&
+                <div className="weather-container">
+                    <div className="main-info-container">
+                        <div className="main-info info-card">
+                            <p style={{ fontSize: '72px' }}>
+                                {props.weather.weatherInfo[0].temperature.curr}°
+                            </p>
+                            <div>
+                                <WeatherEventIcon weatherEvent={props.weather.weatherInfo[0].phenomen} />
+                                <p style={{ display: 'flex', justifyContent: 'center', margin: '-3px' }}>
+                                    {props.weather.weatherInfo[0].phenomen}
+                                </p>
+                            </div>
+                        </div>
+                        <div className="main-info info-card">
+                            <p>Ощущается: {props.weather.weatherInfo[0].temperature.feels}°</p>
+                            <p>Макс.: {props.weather.weatherInfo[0].temperature.max}°</p>
+                            <p>Мин.: {props.weather.weatherInfo[0].temperature.min}°</p>
+                        </div>
+                    </div>
+                    <div className="optional-info">
+                        <div className="info-card">
+                            <div className="optional-info-content">
+                                <WeatherPararmeterIcon weatherParameter='sunrise' />
+                                <p>{props.weather.weatherInfo[0].sun.rise}</p>
+                            </div>
+                            <div className="optional-info-content">
+                                <WeatherPararmeterIcon weatherParameter='sunset' />
+                                <p>{props.weather.weatherInfo[0].sun.set}</p>
+                            </div>
+                        </div>
+                        <div className="info-card">
+                            <div className="optional-info-content">
+                                <WeatherPararmeterIcon weatherParameter='pressure' />
+                                <p>{props.weather.weatherInfo[0].pressure}(ГПа)</p>
+                            </div>
+                            <div className="optional-info-content">
+                                <WeatherPararmeterIcon weatherParameter='humidity' />
+                                <p>{props.weather.weatherInfo[0].humidity}%</p>
+                            </div>
+                        </div>
+                        <div className="info-card">
+                            <div className="optional-info-content">
+                                <WeatherPararmeterIcon weatherParameter='windSpeed'/>
+                                <p>{props.weather.weatherInfo[0].wind.speed} м/c ({props.weather.weatherInfo[0].wind.gust} м/c)</p>
+                            </div>
+                            <div className="optional-info-content">
+                                <WeatherPararmeterIcon weatherParameter='windDir'/>
+                                <p>{props.weather.weatherInfo[0].wind.direction}</p>
+                            </div>
+                        </div>
+                        <div className="info-card">
+                            <div className="optional-info-content">
+                                <WeatherPararmeterIcon weatherParameter='precip' />
+                                <p>{props.weather.weatherInfo[0].precipitations}%</p>
+                            </div>
+                            <div className="optional-info-content">
+                                <WeatherPararmeterIcon weatherParameter='uv' />
+                                <p>{props.weather.weatherInfo[0].uv}</p>
+                            </div>
+                        </div>
+                        <div className="info-card">
+                            <div className="optional-info-content">
+                                <WeatherPararmeterIcon weatherParameter='cloud' />
+                                <p>{props.weather.weatherInfo[0].clouds}%</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div>
+                        Заглушка: выбор дня для прогноза
+                    </div>
                 </div>
-                <div className="optional-info">
-                    <div className="optional-info-child">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" className="bi bi-sunrise" viewBox="0 0 16 16">
-                            <path d="M7.646 1.146a.5.5 0 0 1 .708 0l1.5 1.5a.5.5 0 0 1-.708.708L8.5 2.707V4.5a.5.5 0 0 1-1 0V2.707l-.646.647a.5.5 0 1 1-.708-.708l1.5-1.5zM2.343 4.343a.5.5 0 0 1 .707 0l1.414 1.414a.5.5 0 0 1-.707.707L2.343 5.05a.5.5 0 0 1 0-.707zm11.314 0a.5.5 0 0 1 0 .707l-1.414 1.414a.5.5 0 1 1-.707-.707l1.414-1.414a.5.5 0 0 1 .707 0zM8 7a3 3 0 0 1 2.599 4.5H5.4A3 3 0 0 1 8 7zm3.71 4.5a4 4 0 1 0-7.418 0H.499a.5.5 0 0 0 0 1h15a.5.5 0 0 0 0-1h-3.79zM0 10a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 0 1h-2A.5.5 0 0 1 0 10zm13 0a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 0 1h-2a.5.5 0 0 1-.5-.5z" />
-                        </svg>
-                        <p>{props.sunrise}</p>
-                    </div>
-                    <div className="optional-info-child">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-sunset" viewBox="0 0 16 16">
-                            <path d="M7.646 4.854a.5.5 0 0 0 .708 0l1.5-1.5a.5.5 0 0 0-.708-.708l-.646.647V1.5a.5.5 0 0 0-1 0v1.793l-.646-.647a.5.5 0 1 0-.708.708l1.5 1.5zm-5.303-.51a.5.5 0 0 1 .707 0l1.414 1.413a.5.5 0 0 1-.707.707L2.343 5.05a.5.5 0 0 1 0-.707zm11.314 0a.5.5 0 0 1 0 .706l-1.414 1.414a.5.5 0 1 1-.707-.707l1.414-1.414a.5.5 0 0 1 .707 0zM8 7a3 3 0 0 1 2.599 4.5H5.4A3 3 0 0 1 8 7zm3.71 4.5a4 4 0 1 0-7.418 0H.499a.5.5 0 0 0 0 1h15a.5.5 0 0 0 0-1h-3.79zM0 10a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 0 1h-2A.5.5 0 0 1 0 10zm13 0a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 0 1h-2a.5.5 0 0 1-.5-.5z" />
-                        </svg>
-                        <p>{props.sunset}</p>
-                    </div>
-                    <div className="optional-info-child">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-moisture" viewBox="0 0 16 16">
-                            <path d="M13.5 0a.5.5 0 0 0 0 1H15v2.75h-.5a.5.5 0 0 0 0 1h.5V7.5h-1.5a.5.5 0 0 0 0 1H15v2.75h-.5a.5.5 0 0 0 0 1h.5V15h-1.5a.5.5 0 0 0 0 1h2a.5.5 0 0 0 .5-.5V.5a.5.5 0 0 0-.5-.5h-2zM7 1.5l.364-.343a.5.5 0 0 0-.728 0l-.002.002-.006.007-.022.023-.08.088a28.458 28.458 0 0 0-1.274 1.517c-.769.983-1.714 2.325-2.385 3.727C2.368 7.564 2 8.682 2 9.733 2 12.614 4.212 15 7 15s5-2.386 5-5.267c0-1.05-.368-2.169-.867-3.212-.671-1.402-1.616-2.744-2.385-3.727a28.458 28.458 0 0 0-1.354-1.605l-.022-.023-.006-.007-.002-.001L7 1.5zm0 0-.364-.343L7 1.5zm-.016.766L7 2.247l.016.019c.24.274.572.667.944 1.144.611.781 1.32 1.776 1.901 2.827H4.14c.58-1.051 1.29-2.046 1.9-2.827.373-.477.706-.87.945-1.144zM3 9.733c0-.755.244-1.612.638-2.496h6.724c.395.884.638 1.741.638 2.496C11 12.117 9.182 14 7 14s-4-1.883-4-4.267z" />
-                        </svg>
-                        <p>{props.windSpeed}</p>
-                    </div>
-                    <div className="optional-info-child">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-wind" viewBox="0 0 16 16">
-                            <path d="M12.5 2A2.5 2.5 0 0 0 10 4.5a.5.5 0 0 1-1 0A3.5 3.5 0 1 1 12.5 8H.5a.5.5 0 0 1 0-1h12a2.5 2.5 0 0 0 0-5zm-7 1a1 1 0 0 0-1 1 .5.5 0 0 1-1 0 2 2 0 1 1 2 2h-5a.5.5 0 0 1 0-1h5a1 1 0 0 0 0-2zM0 9.5A.5.5 0 0 1 .5 9h10.042a3 3 0 1 1-3 3 .5.5 0 0 1 1 0 2 2 0 1 0 2-2H.5a.5.5 0 0 1-.5-.5z" />
-                        </svg>
-                        <p>{props.windSpeed}</p>
-                    </div>
-                    <div className="optional-info-child">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-cloud-arrow-down" viewBox="0 0 16 16">
-                            <path fill-rule="evenodd" d="M7.646 10.854a.5.5 0 0 0 .708 0l2-2a.5.5 0 0 0-.708-.708L8.5 9.293V5.5a.5.5 0 0 0-1 0v3.793L6.354 8.146a.5.5 0 1 0-.708.708l2 2z" />
-                            <path d="M4.406 3.342A5.53 5.53 0 0 1 8 2c2.69 0 4.923 2 5.166 4.579C14.758 6.804 16 8.137 16 9.773 16 11.569 14.502 13 12.687 13H3.781C1.708 13 0 11.366 0 9.318c0-1.763 1.266-3.223 2.942-3.593.143-.863.698-1.723 1.464-2.383zm.653.757c-.757.653-1.153 1.44-1.153 2.056v.448l-.445.049C2.064 6.805 1 7.952 1 9.318 1 10.785 2.23 12 3.781 12h8.906C13.98 12 15 10.988 15 9.773c0-1.216-1.02-2.228-2.313-2.228h-.5v-.5C12.188 4.825 10.328 3 8 3a4.53 4.53 0 0 0-2.941 1.1z" />
-                        </svg>
-                        <p>{props.pressure}</p>
-                    </div>
-                </div>
-            </div>
-        }
-        <p>{props.error}</p>
-    </div>
-)
+            }
+            < p > {props.weather.error}</p>
+        </div >
+    )
+}
 
 export default Weather;
